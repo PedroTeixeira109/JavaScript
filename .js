23df7f8438abc7e890638e86b01a0141beed79e5
PedroTeixeira109 <pedroteixeira.7@hotmail.com>
2021-04-16 16:52:52 +0100

    done
    done

---
+++ .js
@@ -0,0 +1,37 @@
+let writeOuput = function(text)
+{
+    document.write(<h1>${text}</h1>)
+}
+writeOuput ("Done!");
+
+class Mamifero
+{
+    constructor(name)
+    { 
+        this.name = name;
+    }
+    DrinkMilk()
+    {
+        return "Look at me, i'm driking milk"
+    }
+}
+class Bear extends Mamifero 
+{
+    constructor(name)
+    {
+        super(name);
+    }
+    DrinkMilk() 
+    {
+        return"look at me, i'm eat meat"
+    }
+}
+
+let Mamifero = new Mamifero("Lucky");
+let Bear = new Bear("Vodka")
+
+for (let i= 0; i <Mamifero.length; i++)
+{
+    writeOuput(Mamifero[i].name);
+    writeOuput(Mamifero[i].DrinkMilk);
+}
